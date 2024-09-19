3bc9107764e66075eff8f3415b82de53d803d4d8
DikamogeloM <dikamogelom@gmail.com>
2024-09-20 01:57:31 +0200

    Added synonyms to dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,14 +8,12 @@ export default function Dictionary() {
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
     setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-
     axios.get(apiUrl).then(handleResponse);
   }
 
