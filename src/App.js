c9f7286b3d7a129ec4116b4893bc9a44b6d788cc
DikamogeloM <dikamogelom@gmail.com>
2024-09-19 08:59:45 +0200

    Initial commit with changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Hello from React</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
     </div>
   );
 }
-
-export default App;
