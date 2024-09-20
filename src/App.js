1814642a65471694df1a23f5d5c79426b89ed910
DikamogeloM <dikamogelom@gmail.com>
2024-09-20 13:27:54 +0200

    Updated project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.png";
-import Dictionary from "./Dictionary";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,7 +4,7 @@ import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState(null);
+  let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
@@ -13,7 +13,9 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
